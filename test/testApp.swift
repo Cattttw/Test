dcb0221608838ee08e11f34b585b221d0da8c543
Cattttw <e0492489@u.nus.edu>
2021-05-27 19:49:20 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Qianyi Wang on 2021/5/27.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
